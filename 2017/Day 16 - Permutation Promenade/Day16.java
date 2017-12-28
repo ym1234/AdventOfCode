12f13f4c4e64ac885ad17e561161ba6ddbc8b927
ym123 <mohamed.yoyo100@gmail.com>
2017-12-28 12:41:01 +0200

    e

--- 2017/Day 16 - Permutation Promenade/Day16.java
+++ 2017/Day 16 - Permutation Promenade/Day16.java
@@ -4,16 +4,16 @@ import java.util.function.*;
 import java.nio.file.*;
 public class Day16 {
 
-	public static class Move {
-		BiFunction <Integer, Integer, Function<List<Character>, Character>> func;
-		public final int[] params;
-		public Move(BiFunction<Integer, Integer, Function<List<Character>, Character>> func, int... params) {
+	public static class Move<T> {
+		public final BiFunction <T, T, Function<List<Character>, T>> func;
+		public final T[] params;
+		public Move(BiFunction<T, T, Function<List<Character>, T>> func, T... params) {
 			this.func = func;
 			this.params = params;
 		}
 	}
 
-	public static void main(String[] args)throws Exception {
+	public static void main(String[] args) throws Exception {
 		Move[] moves = parse(Files.readAllLines(Paths.get(args[0])));
 		while (true) {
 			long millis = System.currentTimeMillis();
@@ -24,14 +24,13 @@ public class Day16 {
 	}
 
 	public static Move[] parse(List<String> file) {
-		return file.parallelStream().flatMap(i -> Arrays.stream(i.split(","))).map(i -> {
+		return file.stream().flatMap(i -> Arrays.stream(i.split(","))).map(i -> {
 			switch (i.charAt(0)) {
-			case 's' : return new Move((x, y) -> list -> { Collections.rotate(list, x); return list.get(x); }, Integer.parseInt(i.substring(1, i.length())), 0);
+			case 's' : return new Move<Integer>((x, y) -> list -> { Collections.rotate(list, x); return x; }, Integer.parseInt(i.substring(1, i.length())), 0);
 			case 'x' :
 				String[] param = i.substring(1, i.length()).split("/");
-				return new Move((x, y) -> list -> list.set(x, list.set(y, list.get(x))), Integer.parseInt(param[0]), Integer.parseInt(param[1]));
-			// auto-unboxing hates me.
-			case 'p' : return new Move((x, y) -> list -> list.set(list.indexOf((char) x.intValue()), list.set(list.indexOf((char) y.intValue()), (char) x.intValue())), i.charAt(1), i.charAt(3));
+				return new Move<Integer>((x, y) -> list -> (int)list.set(x, list.set(y, list.get(x))), Integer.parseInt(param[0]), Integer.parseInt(param[1]));
+			case 'p' : return new Move<Character>((x, y) -> list -> list.set(list.indexOf(x), list.set(list.indexOf(y), x)), i.charAt(1), i.charAt(3));
 			default: return null;
 			}
 		}).toArray(Move[]::new);
@@ -43,7 +42,7 @@ public class Day16 {
 
 	public static List<Character> part2(Move[] moves) {
 		HashMap <List<Character>, Integer> set = new HashMap<>();
-		List<Character> input = Arrays.<Character>asList(new Character[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'});
+		List<Character> input = Arrays.asList(new Character[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'});
 		for (int i = 0; i < 1000000000; i++) {
 			dance(moves, input);
 			if (set.containsKey(input)) {
@@ -56,7 +55,7 @@ public class Day16 {
 	}
 
 	public static List<Character> dance(Move[] moves, List<Character> input) {
-		Arrays.stream(moves).forEach(i -> i.func.apply(i.params[0], i.params[1]).apply(input));
+		Arrays.stream(moves).forEach(i -> ((Function)i.func.apply(i.params[0], i.params[1])).apply(input));
 		/*for (Move move : moves) {
 			switch (move.type) {
 			case 's' : Spin(move.params[0], input); break;
